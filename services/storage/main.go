8a9b3d616d633f021dcdf8dabd91078f0eb63434
ChrolloLucii <jammbeast69@mail.ru>
2025-10-03 17:30:43 +0300

    test: Hello world

--- services/storage/main.go
+++ services/storage/main.go
@@ -1,3 +1,10 @@
 package main
 
-// TODO: Реализовать Storage Service (взаимодействие с MongoDB)
+import "fmt"
+
+func HelloWorld() string {
+	return "Hello World from Storage Service!"
+}
+func main() {
+	fmt.Println(HelloWorld())
+}
